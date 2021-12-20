211740ea42d1db025bc511187d3c0e373c9ce826
IngAmaury <92232156+IngAmaury@users.noreply.github.com>
2021-12-20 02:10:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,5 +104,5 @@ def AS(input_text):
 ############__________WEBAPP_______###################
 st.title("Hola soy Psibot")
 txt = st.text_area('Introduce lo que me quieres contar')
-as=AS(txt);
+as=AS(txt)
 st.write('Sentimentos:',as[1],as[2])
