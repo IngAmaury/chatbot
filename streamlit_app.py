655bcfae8e6a7f5b4c221c7ad0b02c98e1b49a8c
IngAmaury <92232156+IngAmaury@users.noreply.github.com>
2021-12-20 02:05:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,5 +101,14 @@ def AS(input_text):
   #S=wc_result
   plt.show()
   return (re1,re2,S)
-#############
+############__________WEBAPP_______###################
 st.title("Hola soy Psibot")
+st.text('Introduce lo que me quieres contar')
+txt = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
