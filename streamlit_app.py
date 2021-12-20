afa13add342278ee579ed435149ecf2bc4fc3815
IngAmaury <92232156+IngAmaury@users.noreply.github.com>
2021-12-20 01:19:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+#lIBRERIA DE LAWEB APP:
 import streamlit as st
 ##Cargamos las librerias necesarias para trabajar los datos
 #import re
@@ -12,11 +13,10 @@ nltk.download('stopwords')
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import pandas as pd
-import io
+#import matplotlib.pyplot as plt
+#import os
+#import pandas as pd
+#import io
 ##Embedding tf2-preview-nnlm https://tfhub.dev/google/collections/tf2-preview-nnlm/1
 #https://tfhub.dev/google/collections/nnlm/1
 #embedd = hub.load("https://tfhub.dev/google/tf2-preview/nnlm-de-dim128-with-normalization/1")
