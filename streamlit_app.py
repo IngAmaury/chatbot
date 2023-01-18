c69c61e19ed4caaed872a445cdba20072699bff5
Amaury Santiago Horta <asantiagoh1501@alumno.ipn.mx>
2023-01-17 19:22:30 -0600

    Reducido uso de memoria al importar librerías sólo cuando es necesario.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 import nltk
 import numpy as np
 import streamlit as st  # librería de la webapp
-import tensorflow_hub as hub
 from nltk import WordPunctTokenizer
 from nltk.corpus import stopwords
 from unidecode import unidecode
-from wordcloud import WordCloud
 
 # datos persistentes entre sesiones de streamlit
 if 'embedd128' not in st.session_state:
+    import tensorflow_hub as hub
+
     # tokens entrenados a partir de Google News en Español
     st.session_state['embedd128'] = hub.load(
         "https://tfhub.dev/google/tf2-preview/nnlm-es-dim128-with-normalization/1"
@@ -25,9 +25,6 @@ try:
     nltk.data.find('stopwords')
 except LookupError:
     nltk.download('stopwords', quiet=True)
-
-WPT = WordPunctTokenizer()
-wc = WordCloud()
 
 # Diccionarios para evaluar las predicciones de los modelos
 polaridad = {0: 'Positivos', 1: 'Negativos'}
@@ -48,7 +45,7 @@ def text_to_matrix(input):
     sanitized = unidecode(string.lower())
 
     # tokenizar
-    tokens = WPT.tokenize(sanitized)
+    tokens = WordPunctTokenizer().tokenize(sanitized)
 
     # eliminar datos redundates de los tokens (stopwords y símbolos):
     limpio = tokens[:]
@@ -83,7 +80,7 @@ def text_to_wordcloud(inp):
     """
 
     # Tokenizar
-    toks = WPT.tokenize(inp)
+    toks = WordPunctTokenizer().tokenize(inp)
     limpio = toks[:]
     # Limpiar los tokens:
     for tokens in toks:
@@ -158,7 +155,9 @@ def main():
             )
 
             # Nube de palabras
-            word_cloud = wc.generate(text_to_wordcloud(text_input))
+            from wordcloud import WordCloud
+            
+            word_cloud = WordCloud().generate(text_to_wordcloud(text_input))
             plt.style.use('dark_background')
             plt.axis('off')
             plt.imshow(word_cloud, interpolation='bicubic')
